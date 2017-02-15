ddc0466eb8ace0534fc9510794da184e993efba6
Rudy Chung <rudyboy.tw@gmail.com>
2017-02-15 09:35:36 +0800

    made instance selection in one place

--- jenkins/build-script.sh
+++ jenkins/build-script.sh
@@ -1,18 +1,19 @@
 build=NG
+instance=ttb-hd
 [ -z "$DROPBOX" ] && DROPBOX=/var/shared/Dropbox
 
 ## start Google GCE for taiwan topo building
-gcloud compute instances start taiwan-topo-builder
+gcloud compute instances start ${instance}
 for i in 1 2 3; do 
-  gcloud compute ssh --command="~/bin/build.sh" rudychung@taiwan-topo-builder && build=OK && break
+  gcloud compute ssh --command="~/bin/build.sh" rudychung@${instance} && build=OK && break
   
   ## failed, let's retry
-  gcloud compute instances stop taiwan-topo-builder
+  gcloud compute instances stop ${instance}
   sleep $((i*60))
-  gcloud compute instances start taiwan-topo-builder
+  gcloud compute instances start ${instance}
 done || echo "Failed after retry 3 times."
 
-gcloud compute instances stop taiwan-topo-builder
+gcloud compute instances stop ${instance}
 
 
 ## Deploy to Dropbox
