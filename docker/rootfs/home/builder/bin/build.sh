f8931b107c01633332af924ccdb6b8082a43b890
Rudy Chung <rudyboy.tw@gmail.com>
2017-06-10 00:16:27 +0800

    used rclone to remove the need of home server

--- docker/rootfs/home/builder/bin/build.sh
+++ docker/rootfs/home/builder/bin/build.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 set -ex
 
+PATH=~/bin:$PATH
+
 export INSTALL_DIR=v$(date +%Y.%m.%d)
 GS_FILENAME="dayofmonth_$(date +%d)"
-GS_BUCKET="gs://osm-twmap-drops/"
+DAYOFWEEK="$(date +%w)"
 
 cd ~/taiwan-topo
 
+git clean -fdx
 git pull --rebase
 
 make distclean
@@ -14,10 +17,26 @@ make SUITE=taiwan all
 make SUITE=taiwan_lite all
 make SUITE=taiwan_bw all
 make SUITE=taiwan_odc all
+make SUITE=taiwan_bw_dem all
+make SUITE=taiwan_odc_dem all
 
 rm -rf install/${INSTALL_DIR}
 mkdir -p install/${INSTALL_DIR}
 make INSTALL_DIR=install/${INSTALL_DIR} drop
-tar czf install/${GS_FILENAME}.tgz -C install ${INSTALL_DIR}
+cd install/${INSTALL_DIR}
+tree -L 1 -H . | sed -e 's,<br>.*href="\./.*/".*</a>.*<br>,<br>,' -e 's,<a .*href="\.".*>\.</a>,,' > files.html
 
-gsutil cp install/${GS_FILENAME}.tgz ${GS_BUCKET}
+## rclone to dropbox
+rclone copy . rudybox:Public/drops/
+[ "${DAYOFWEEK}" -eq 4 ] && {
+    rclone copy . rudybox:Public/maps/
+    mkdir -p share-mapdata
+    cp -r MOI_OSM_* share-mapdata/
+    rclone copy share-mapdata/ rudybox:Apps/share-mapdata/
+    echo "Completed with weeekly drop."
+} || echo "Completed without weekly drop."
+  
+
+### put to google bucket
+#tar czf install/${GS_FILENAME}.tgz -C install ${INSTALL_DIR}
+#gsutil cp install/${GS_FILENAME}.tgz gs://osm-twmap-drops/
--- jenkins/build-script.sh
+++ jenkins/build-script.sh
@@ -1,6 +1,5 @@
 build=NG
 instance=ttb-hd
-[ -n "$DROPBOX" ] || { echo "DROPBOX path is not specified."; exit 1; }
 
 ## start Google GCE for taiwan topo building
 gcloud compute instances start ${instance}
@@ -17,19 +16,4 @@ gcloud compute instances stop ${instance}
 
 
 ## Deploy to Dropbox
-[ "${build}" = "OK" ] || exit 1
-
-VERSION=$(date +%Y.%m.%d)
-GS_FILENAME="dayofmonth_$(date +%d)"
-
-gsutil cp gs://osm-twmap-drops/${GS_FILENAME}.tgz .
-tar xzf ${GS_FILENAME}.tgz
-cp -r v${VERSION}/* $DROPBOX/Public/drops/  ### daily drops
-
-[ "$(date +%w)" -eq 4 ] && {  ### weekly drops
-  cp -r v${VERSION}/* $DROPBOX/Public/maps/
-  cp -r v${VERSION}/MOI_OSM_* $DROPBOX/Apps/share-mapdata/
-  cd $DROPBOX/Public/maps/
-  tree -L 1 -H . | sed -e 's,<br>.*href="\./.*/".*</a>.*<br>,<br>,' -e 's,<a .*href="\.".*>\.</a>,,' > files.html
-  echo "Completed with weeekly drop."
-}  || echo "Completed without weekly drop."
+[ "${build}" = "OK" ] && echo "Build Done" || echo "Build Failed"
