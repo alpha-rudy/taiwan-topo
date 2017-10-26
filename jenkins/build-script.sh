aa3653f8ee8e6bf78e35682847ea89f730fbb6e1
Rudy Chung <rudy.chung@liteon.com>
2017-10-26 12:31:36 +0800

    refined retry mechanism to have daily build smoother

--- jenkins/build-script.sh
+++ jenkins/build-script.sh
@@ -3,14 +3,15 @@ instance=ttb-hd
 
 ## start Google GCE for taiwan topo building
 gcloud compute instances start ${instance}
-for i in 1 2 3; do 
+
+for i in {1..5}; do 
   gcloud compute ssh --command="~/bin/build.sh" rudychung@${instance} && build=OK && break
   
   ## failed, let's retry
   gcloud compute instances stop ${instance}
-  sleep $((i*60))
+  sleep $((i*600))
   gcloud compute instances start ${instance}
-done || echo "Failed after retry 3 times."
+done || echo "Failed after retry 5 times."
 
 gcloud compute instances stop ${instance}
 
