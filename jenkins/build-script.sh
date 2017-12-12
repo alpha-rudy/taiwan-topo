eba36f89be3a3be9eef7199a6c0244bfc63a5e5b
Rudy Chung <alpha-rudy@users.noreply.github.com>
2017-12-12 20:27:31 +0800

    added debug log and sleep double time

--- jenkins/build-script.sh
+++ jenkins/build-script.sh
@@ -1,8 +1,9 @@
+set -ex
 build=NG
 instance=ttb-hd
 
 ## start Google GCE for taiwan topo building
-gcloud compute instances start ${instance} && sleep 15
+gcloud compute instances start ${instance} && sleep 30
 
 for i in {1..5}; do 
   gcloud compute ssh --command="~/bin/build.sh" rudychung@${instance} && build=OK && break
@@ -10,7 +11,7 @@ for i in {1..5}; do
   ## failed, let's retry
   gcloud compute instances stop ${instance}
   sleep $((i*1200))
-  gcloud compute instances start ${instance} && sleep 15
+  gcloud compute instances start ${instance} && sleep 30
 done || echo "Failed after retry 5 times."
 
 gcloud compute instances stop ${instance}
