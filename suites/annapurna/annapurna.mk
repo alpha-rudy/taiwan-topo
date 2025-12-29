1a80cb45d8487ada394cd4860e969633dfe57b67
Rudy Chung <rudy_chung@xavi.com.tw>
2025-12-29 15:24:48 +0000

    added annapurna

---
+++ suites/annapurna/annapurna.mk
@@ -0,0 +1,21 @@
+# Suite: annapurna - Annapurna (Nepal) mapsforge build
+ifeq ($(SUITE),annapurna)
+REGION := Annapurna
+DEM_NAME := AW3D30
+LANG := ne
+CODE_PAGE := 65001
+ELEVATION_FILE = ele_annapurna_10_100_500.pbf
+ELEVATION_MIX_FILE = ele_annapurna_10_100_500_mix.pbf
+EXTRACT_FILE := nepal-latest
+BOUNDING_BOX := true
+LEFT := 83.0
+RIGHT := 85.0
+BOTTOM := 28.0
+TOP := 29.0
+NAME_MAPSFORGE := $(DEM_NAME)_OSM_$(REGION)_TOPO_Rudy
+NAME_CARTO := $(REGION)_carto
+HGT := $(ROOT_DIR)/hgt/annapurna_hgtmix.zip
+GTS_STYLE = $(HS_STYLE)
+TOPO_PAGE := annapurna_topo
+TARGETS := mapsforge_zip poi_zip poi_v2_zip locus_poi_zip gts_all carto_all locus_map
+endif
---
+++ suites/annapurna/annapurna_bc_dem.mk
@@ -0,0 +1,21 @@
+# Suite: annapurna_bc_dem - Annapurna basecamp style with DEM
+ifeq ($(SUITE),annapurna_bc_dem)
+REGION := Annapurna
+DEM_NAME := AW3D30
+LANG := ne
+CODE_PAGE := 65001
+ELEVATION_FILE = ele_annapurna_10_100_500.pbf
+EXTRACT_FILE := nepal-latest
+BOUNDING_BOX := true
+LEFT := 83.0
+RIGHT := 85.0
+BOTTOM := 28.0
+TOP := 29.0
+TYP := basecamp
+LR_STYLE := swisspopo
+HR_STYLE := basecamp
+STYLE_NAME := camp3D
+GMAPDEM := $(ROOT_DIR)/hgt/annapurna_hgtmix.zip
+MAPID := $(shell printf %d 0x1018)
+TARGETS := gmapsupp_zip gmap nsis
+endif
