cdd3e18d89274917b31e39ce106a21c5b80b979a
Rudy Chung <rudy_chung@xavi.com.tw>
2026-01-09 09:30:59 +0000

    added kashmir suites

---
+++ suites/kashmir/kashmir_bc_dem.mk
@@ -0,0 +1,21 @@
+# Suite: kashmir_bc_dem - Kashmir basecamp style with DEM
+ifeq ($(SUITE),kashmir_bc_dem)
+REGION := Kashmir
+DEM_NAME := AW3D30
+LANG := hi
+CODE_PAGE := 65001
+ELEVATION_FILE = ele_kashmir_10_100_500.pbf
+EXTRACT_FILE := india-latest
+BOUNDING_BOX := true
+LEFT := 74.5
+RIGHT := 75.5
+BOTTOM := 34.0
+TOP := 34.75
+TYP := basecamp
+LR_STYLE := swisspopo
+HR_STYLE := basecamp
+STYLE_NAME := camp3D
+GMAPDEM := $(ROOT_DIR)/hgt/kashmir_hgtmix.zip
+MAPID := $(shell printf %d 0x1028)
+TARGETS := gmapsupp_zip gmap nsis
+endif
