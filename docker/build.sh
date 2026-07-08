50aece0d4893a54557eb488e19da3acc0f09bd18
Rudy Chung <rudy_chung@xavi.com.tw>
2026-07-08 15:34:16 +0800

    use docker after then

--- docker/build.sh
+++ docker/build.sh
@@ -14,7 +14,7 @@ while sleep 10; do { date +'DS: %H:%M:%S'; free -h; df -h /; } >> log/mem_$(date
 # background logging is fine, as build machine would be shutdown after building
 
 cd ~/taiwan-topo
-make distclean-extracts
+./tools/docker-make distclean-extracts
 git clean -fd
 git checkout master -- .
 git pull --rebase
@@ -24,22 +24,22 @@ rm -rf ${INSTALL_DIR}
 mkdir -p ${INSTALL_DIR}
 
 if [[ "${TARGET}" == *"suites"* ]]; then
-    make INSTALL_DIR=/workspace/${INSTALL_DIR} ${TARGET}
-    make exps || echo make exps failed
+    ./tools/docker-make INSTALL_DIR=/workspace/${INSTALL_DIR} ${TARGET}
+    ./tools/docker-make exps || echo make exps failed
     cd ${INSTALL_DIR}
     tree -L 1 -H . | sed -e 's,<br>.*href="\./.*/".*</a>.*<br>,<br>,' -e 's,<a .*href="\.".*>\.</a>,,' > files.html
     rclone copy ${RCLONE_OPTS} . rudybox:Apps/share-mapdata/
     echo "Completed with weekly drop."
 elif [ "${TARGET}" == "daily" ]; then
-    make INSTALL_DIR=/workspace/${INSTALL_DIR} ${TARGET}
-    make exps || echo make exps failed
+    ./tools/docker-make INSTALL_DIR=/workspace/${INSTALL_DIR} ${TARGET}
+    ./tools/docker-make exps || echo make exps failed
     cd ${INSTALL_DIR}
     tree -L 1 -H . | sed -e 's,<br>.*href="\./.*/".*</a>.*<br>,<br>,' -e 's,<a .*href="\.".*>\.</a>,,' > files.html
     rclone copy ${RCLONE_OPTS} . rudybox:Apps/share-mapdata/drops/
     echo "Completed with daily drop."
 elif [ "${TARGET}" == "world" ]; then
     for i in annapurna alps_core alps_western alps_eastern alps_fareast elbrus fujisan kashmir kumano nikko_oze; do
-    	make ${i}_suites
+    	./tools/docker-make ${i}_suites
         cd install-${i}
         rclone copy ${RCLONE_OPTS} . rudybox:Apps/share-mapdata/
 	cd ..
